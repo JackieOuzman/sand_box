e468c74202b9db821081dc0a7325d0542d41f80a
Jackie Ouzman <jackie.ouzman@csiro.au>
2019-07-02 12:47:37 +0930

    converting to ripper tool

--- app.R
+++ app.R
@@ -23,9 +23,9 @@ header <-   dashboardHeader()
 #this is the list of all tabs in the app
 sidebar <-  dashboardSidebar(
             sidebarMenu(
-              menuItem("The farm and mitigation", tabName = "tell_me", icon = icon("question")),
-              menuItem("Crop sequence", tabName = "set_up_farm", icon = icon("th")),
-              menuItem("mitigation cost", tabName = "mitigation", icon = icon("list")),
+              menuItem("The farm and ripping", tabName = "tell_me", icon = icon("question")),
+              menuItem("Wheat", tabName = "set_up_farm", icon = icon("th")),
+              menuItem("Ripping cost", tabName = "mitigation", icon = icon("list")),
               menuItem("Commodity prices", tabName = "extra", icon = icon("dollar")),
               menuItem("Results", tabName = "results", icon = icon("bar-chart-o"))
               #,menuItem("test", tabName = "test")
@@ -100,25 +100,10 @@ body <- dashboardBody(
                checkboxGroupInput(
                  "mangement_options",
                  label = h3("What are you considering?"),
-                 choices = list("Wetting agents" = "wetter", 
-                                #"Sowing on edge of row" = "sow_edge", 
-                                #"Spading with no inputs" = "spade_no_inputs", 
-                                #"Spading with shallow organic inputs" = "spade_organic",
-                                #"Spading with shallow fertiliser as inputs" = "spade_fert",
-                                "Ripping with no inputs" = "rip_no_inputs",
-                                "Ripping with shallow organic inputs"= "rip_shallow_organic",
-                                "Ripping with shallow fertiliser as inputs" = "rip_shallow_fert",
-                                "Ripping with deep organic inputs"= "rip_deep_organic",
-                                "Ripping with deep fertiliser as inputs" = "rip_deep_fert"), 
-                 selected = "rip_no_inputs")
-               #radioButtons(
-               #"depth",
-               #label = h4("Depth of ripping?"),
-               #choices = list("Ripping to 30 cm" = "30",
-               #               "Ripping to 50 cm" = "50",
-               #               "Ripping to 60 cm" = "60",
-               #               "Ripping to 70 cm" = "70"),
-               #selected ="30")
+                 choices = list("Shallow ripping with inputs" = "rip_shallow_fert",
+                                "Deep ripping with inputs" = "rip_deep_fert"), 
+                 selected = "rip_shallow_fert")
+              
              ) #well pannel
       ) #column bracket
       
@@ -131,118 +116,38 @@ body <- dashboardBody(
       tabName = "set_up_farm",
       
         
-        #tabsetPanel(
-          #tabPanel(h3("Zone1")), 
-          
-          
-          
-          #numericInput(
-          #  "size_zone1",
-          #  label = h4("Size of zone 1 (ha)"),
-          #  value = 2500, #this needs to be total area of farm /3
-          #  min = 2500,
-          #  max = 6000, #can't be larger than total farm area
-          #  step =100),
-          
-           
-          
-          wellPanel(
-            h4("Crop sequence for 10 years"),
-            h6("Code for crops is:"),
-            #br(),
-            h6("wh =wheat, ba =barley, ca= canola, leg = grain legume , pas =pasture"),
-            br(),
-            selectizeInput("crop_seq_zone1", 
-                           label="Crop type:",
-                           choices =list(
-                             "Year1"= c("Yr1_wh","Yr1_ba","Yr1_can","Yr1_leg","Yr1_pas"),
-                             "Year2"= c("Yr2_wh","Yr2_ba","Yr2_can","Yr2_leg","Yr2_pas"),
-                             "Year3"= c("Yr3_wh","Yr3_ba","Yr3_can","Yr3_leg","Yr3_pas"),
-                             "Year4"= c("Yr4_wh","Yr4_ba","Yr4_can","Yr4_leg","Yr4_pas"),
-                             "Year5"= c("Yr5_wh","Yr5_ba","Yr5_can","Yr5_leg","Yr5_pas"),
-                             "Year6"= c("Yr6_wh","Yr6_ba","Yr6_can","Yr6_leg","Yr6_pas"),
-                             "Year7"= c("Yr7_wh","Yr7_ba","Yr7_can","Yr7_leg","Yr7_pas"),
-                             "Year8"= c("Yr8_wh","Yr8_ba","Yr8_can","Yr8_leg","Yr8_pas"),
-                             "Year9"= c("Yr9_wh","Yr9_ba","Yr9_can","Yr9_leg","Yr9_pas"),
-                             "Year10"= c("Yr10_wh","Yr10_ba","Yr10_can","Yr10_leg","Yr10_pas")
-                           ),
-                           selected = c("Yr1_wh", "Yr2_wh", "Yr3_wh" ,"Yr4_wh" ,"Yr5_wh", "Yr6_wh", "Yr7_wh", "Yr8_wh" ,"Yr9_wh", "Yr10_wh"),
-                           options = list(maxItems = 10),
-                           multiple = TRUE
-            ),
+            wellPanel(
+            h4("Wheat crop"),
            
             #textOutput("text_size_farm"), # I want to move this up so its above select                   
            
             br(),
             
             
-            fluidRow(column(6,
-            
+           
            numericInput("aa",
-                        label = "Current wheat (t/ha)",
+                        label = "P5 for wheat yield (t/ha)",
                         value = 3,
                         min = 0,
                         max= 8,
                         step = 0.5),
            numericInput("bb",
-                        label = "Current barley (t/ha)",
+                        label = "P50 for wheat yield  (t/ha)",
                         value = 3,
                         min = 0,
                         max= 8,
                         step = 0.5),
            numericInput("cc",
-                        label = "Current canola (t/ha)",
-                        value = 3,
-                        min = 0,
-                        max= 8,
-                        step = 0.5),
-           numericInput("dd",
-                        label = "Current legume (t/ha)",
+                        label = "P90 for wheat yield  (t/ha)",
                         value = 3,
                         min = 0,
                         max= 8,
                         step = 0.5)
-            ), #column bracket
            
-           column(6,
            
-           numericInput("aaa",
-                        label = "Potential wheat (t/ha)",
-                        value = 4,
-                        min = 0,
-                        max= 8,
-                        step = 0.5),
-           numericInput("bbb",
-                        label = "Potential barley (t/ha)",
-                        value = 4,
-                        min = 0,
-                        max= 8,
-                        step = 0.5),
-           numericInput("ccc",
-                        label = "Potential canola (t/ha)",
-                        value = 4,
-                        min = 0,
-                        max= 8,
-                        step = 0.5),
-           numericInput("ddd",
-                        label = "Potential legume (t/ha)",
-                        value = 4,
-                        min = 0,
-                        max= 8,
-                        step = 0.5)
-           ) #column bracket
-            ) #fluidRow bracket
-          
-          #Add these (zone2 and zone3) in later when I work out the details
-          #they will look like zone 1
-          #tabPanel(h3("Zone2")),
-          
-          #tabPanel(h3("Zone3"))
-          #) 
           
       ), #well pannel bracket
-      valueBoxOutput("yld_pot_wheat"),
-      valueBoxOutput("yld_pot_pulses")
+      valueBoxOutput("yld_pot_wheat")
       ),
     
     
